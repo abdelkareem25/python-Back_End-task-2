1e05b280a28f18f3d96832bdfc23ee4a5899335b
abdelkareem25 <abdelkareemhany38@gmail.com>
2022-08-13 13:04:53 +0200

    finshed task

---
+++ python task.py
@@ -0,0 +1,172 @@
+
+x=12
+print(bool(x))
+#or
+xx=14
+print(xx==14)
+
+print('---------------------')
+
+y = 10
+print(y)
+
+print('---------------------')
+
+z=25.4
+print(z)
+
+print('---------------------')
+
+s="after this course i will be a python developer"
+print(s)
+
+print('---------------------')
+
+q=15
+print(float(q))
+print('---------------------')
+
+#No 
+print('---------------------')
+l=[1,2,3,4,5]
+print(l)
+print('---------------------')
+
+tup=(10,11,12,13,14,15)
+print(tup)
+print('---------------------')
+L2=[2,5,8,9,6,3]
+print(tuple(L2))
+print('---------------------')
+
+DICT={"abdo":1,"khaled":2,"badr":3}
+print(DICT)
+print('---------------------')
+
+#No but in one case we can use semi colon
+print('---------------------')
+#interpreted
+print('---------------------')
+#I can't understand this question
+
+print('---------------------')
+# = it is mean equal
+# == it is mean is this variable this value?
+
+print('---------------------')
+# not equal
+
+print('---------------------')
+
+X=10
+X+=15
+print(X)
+
+print('---------------------')
+
+X/=5
+print(X)
+print('---------------------')
+
+X*=10
+print(X)
+print('---------------------')
+X%=3
+print(X)
+
+print('---------------------')
+print(11%4)
+print('---------------------')
+print(2**3)
+print('---------------------')
+print(11//3)
+
+print('---------------------')
+#yes when we use //
+print('---------------------')
+Z=10
+Y=15
+print(Z>Y)
+if Z<Y:
+    print("10 smaller than 15")
+else:
+    print("10 it's bigger than 15")
+    
+print('---------------------')
+#in if statment
+print('---------------------')
+#There is no difference
+
+print('---------------------')
+
+print('---------------------')
+#all
+print('---------------------')
+#if it's the first condition statment
+#elif the secund
+print('---------------------')
+#elif it's the first condition statment and we can use many elif in one statment
+#else it's therd statment and we can use one only
+print('---------------------')
+#yes
+print('---------------------')
+#No
+print('---------------------')
+F = 20
+
+print('---------------------')
+#False 
+print('---------------------')
+#Finsh the if statment
+print('---------------------')
+M=[2,4,6,8,10]
+if 4 in M:
+    print('yes')
+else :
+    print('no')
+if 4 and 6 in M:
+    print('Yes')
+else:
+        print('NOOO')
+
+if 3 or 6 in M:
+    print('One piece is the best anime ever')
+else:
+    print("kufrvbkus")
+'''if any ([3,6])in M:
+    print ("One piece is the best anime ever")
+else:
+    print('Hunter X Hunter the socund')
+-----------.why this code not working.------------------------
+'''
+print('---------------------')
+if 2 and 4 and 5 in M:
+    print('eng/mahmoud')
+else :
+    print('Eng:Mahmoud')
+print('---------------------')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
